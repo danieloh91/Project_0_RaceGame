48d5a872d749315ed9b54948345efddb645b14fa
danieloh91 <daniel.oh.91@gmail.com>
2016-07-09 20:24:41 -0700

    erase table, set fishes on html, create login that hides

--- main.js
+++ main.js
@@ -1,3 +1,29 @@
-$(document).on('ready', function() {
+// $(document).on('ready', function() {
+//   $(window).keypress(function() {
+//     $('.racecar1').animate({ "left": "+=50px" }, "slow" );
+//   });
+// });
+// // jquery animate
 
+$(document).on('ready', function(){
+  visible = true;
+  //hide Player 1 after submit
+  $(".button1").click(function(){
+    if (visible) {
+      $(".player1").slideUp('slow',function(){
+        $(".player1").addClass('hide').slideDown(0);
+      });
+    }
+    visible = ! visible;
+  });
+  //hide Player 2 after submit
+  $(".button2").click(function(){
+    if (visible) {
+      $(".player2").slideUp('fast',function(){
+        $(".player2").addClass('hide').slideDown(0);
+      });
+    }
+    visible = ! visible;
+  });
+  console.log('swim!');
 });
