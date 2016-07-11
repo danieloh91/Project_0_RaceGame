7413246e464ba08ce7094401685ab3c8c6cbba5d
danieloh91 <daniel.oh.91@gmail.com>
2016-07-10 19:41:06 -0700

    determine winner based on # of key press and show winner pane

--- main.js
+++ main.js
@@ -21,42 +21,53 @@ $(document).on('ready', function(){
 
   var nemo = new Fish();
   var dori = new Fish();
+
+  var $winner = function(){
+    $('.winPopUp').show(100).append('<p>Congrats!</p><br>' + this.name + ' wins!');
+  };
+
   //press Z or / to make nemo or dori move to the right
   $("body").keypress(function(event){
     if (event.keyCode === 122) {
       $('.swimOne').animate({"margin-left": "+=4%" }, "fast" );
+      nemo.name = nemo;
       nemo.lap();
       if(nemo.stroke===5) {
         // $('.board-wrapper').append('<div class="overlay"></div>');
         // $('.overlay').append('<p>Nemo Wins!</p>');
         // alert("nemo wins!");
-        $('.winPopUp').show(250);
+        return $winner();
       }
     } else if (event.keyCode === 47) {
       $('.swimTwo').animate({"margin-left": "+=4%" }, "fast" );
+      dori.name = dori;
       dori.lap();
       if(dori.stroke===5) {
-        alert("dori wins!");
+        $('.winPopUp').show(100);
+        return $winner();
       }
     }
   });
-  initMap();
+  // initMap();
 });
 
 function Fish() {
   this.stroke = 0;
+  this.name = null;
   this.lap = function() {
     this.stroke++;
   };
 }
 
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 13, lng: -100},
-    zoom: 3
-  });
-  var marker = new google.maps.Marker({
-    position: {lat: 35, lng: -40},
-    map: map,
-  });
-}
+
+
+// function initMap() {
+//   map = new google.maps.Map(document.getElementById('map'), {
+//     center: {lat: 13, lng: -100},
+//     zoom: 3
+//   });
+//   var marker = new google.maps.Marker({
+//     position: {lat: 35, lng: -40},
+//     map: map,
+//   });
+// }
