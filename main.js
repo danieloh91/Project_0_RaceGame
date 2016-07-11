f4184c4c691fd9d6e82392637d0f7fb57303d00b
danieloh91 <daniel.oh.91@gmail.com>
2016-07-10 18:53:48 -0700

    tried to overlay gameboard to google maps api but unsuccessful

--- main.js
+++ main.js
@@ -18,12 +18,45 @@ $(document).on('ready', function(){
     }
     visible = ! visible;
   });
+
+  var nemo = new Fish();
+  var dori = new Fish();
   //press Z or / to make nemo or dori move to the right
-  $("html").keypress(function(event){
+  $("body").keypress(function(event){
     if (event.keyCode === 122) {
-      $('.swimOne').animate({"margin-left": "+=50px" }, "fast" );
+      $('.swimOne').animate({"margin-left": "+=4%" }, "fast" );
+      nemo.lap();
+      if(nemo.stroke===5) {
+        // $('.board-wrapper').append('<div class="overlay"></div>');
+        // $('.overlay').append('<p>Nemo Wins!</p>');
+        // alert("nemo wins!");
+        $('.winPopUp').show(250);
+      }
     } else if (event.keyCode === 47) {
-      $('.swimTwo').animate({"margin-left": "+=50px" }, "fast" );
+      $('.swimTwo').animate({"margin-left": "+=4%" }, "fast" );
+      dori.lap();
+      if(dori.stroke===5) {
+        alert("dori wins!");
+      }
     }
   });
+  initMap();
 });
+
+function Fish() {
+  this.stroke = 0;
+  this.lap = function() {
+    this.stroke++;
+  };
+}
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 13, lng: -100},
+    zoom: 3
+  });
+  var marker = new google.maps.Marker({
+    position: {lat: 35, lng: -40},
+    map: map,
+  });
+}
