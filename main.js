e76eb522e39626335f2f6beb9995644f32d551e5
danieloh91 <daniel.oh.91@gmail.com>
2016-07-11 01:17:43 -0700

    remove google maps api, add background and finish line

--- main.js
+++ main.js
@@ -21,29 +21,29 @@ $(document).on('ready', function(){
 
   var dori = new Fish();
   var nemo = new Fish();
-  // var $winner = function(){
-  //   $('.winPopUp').show(100).append(this.name);
-  // };
 
   //press Z or / to make nemo or dori move to the right
   $("body").keypress(function(event){
     if (event.keyCode === 122) {
-      $('.swimOne').animate({"margin-left": "+=4%" }, "fast" );
+      $('.swimOne').animate({"margin-left": "+=4%"}, "fast" );
       nemo.name = 'Nemo';
       nemo.lap();
       if(nemo.stroke===5) {
-        return $('.winPopUp').show(100).append('<p>Congrats!<br>' + nemo.name + ' wins!</p>');
+        $('.winPopUp').show(100).append('<p>Congrats!<br>' + nemo.name + ' wins!</p>');
+        $('.swimOne').clearQueue();
+        $('.swimOne').stop();
       }
     } else if (event.keyCode === 47) {
-      $('.swimTwo').animate({"margin-left": "+=4%" }, "fast" );
+      $('.swimTwo').animate({"margin-left": "+=4%"}, "fast" );
       dori.name = 'Dori';
       dori.lap();
       if(dori.stroke===5) {
-        return $('.winPopUp').show(100).append('<p>Congrats!<br>' + dori.name + ' wins!</p>');
+        $('.winPopUp').show(100).append('<p>Congrats!<br>' + dori.name + ' wins!</p>');
+        $('.swimTwo').clearQueue();
+        $('.swimTwo').stop();
       }
     }
   });
-  // initMap();
 });
 
 function Fish() {
@@ -53,16 +53,3 @@ function Fish() {
     this.stroke++;
   };
 }
-
-
-
-// function initMap() {
-//   map = new google.maps.Map(document.getElementById('map'), {
-//     center: {lat: 13, lng: -100},
-//     zoom: 3
-//   });
-//   var marker = new google.maps.Marker({
-//     position: {lat: 35, lng: -40},
-//     map: map,
-//   });
-// }
