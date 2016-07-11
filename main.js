d5032464f9c6ab7eebbdd2424f227eff570c5ee5
danieloh91 <daniel.oh.91@gmail.com>
2016-07-11 02:48:42 -0700

    add comment, what key to press, and change font colors

--- main.js
+++ main.js
@@ -2,13 +2,16 @@ $(document).on('ready', function(){
   visible = true;
   //choose 1 or 2 players
   $(".select1Player").click(function(){
+    //1 and 2 player buttons hide after selection
     if (visible) {
       $(".choosePlayers").slideUp('slow',function(){
         $(".choosePlayers").addClass('hide').slideDown(0);
       });
     }
     visible = ! visible;
+    //fishes appear after button hides
     $('.fishes').show(500).slideDown(100);
+    //player 2 fish starts to move
     $('.swimTwo').animate({"margin-left": "+=90%"}, 9900);
   });
   $(".select2Player").click(function(){
@@ -30,7 +33,7 @@ $(document).on('ready', function(){
       $('.swimOne').animate({"margin-left": "+=4%"}, "fast" );
       nemo.name = 'Nemo';
       nemo.lap();
-      if(nemo.stroke===5) {
+      if(nemo.stroke===23) {
         $('.winPopUp').show(100).append("<h2>Congrats!<br>" + nemo.name + " wins!</h2><br><a class='btn reset' href='index.html'><strong>New Game<strong></a>");
         $('.swimOne').stop();
         $('.swimTwo').stop();
@@ -39,17 +42,19 @@ $(document).on('ready', function(){
       $('.swimTwo').animate({"margin-left": "+=4%"}, "fast" );
       dori.name = 'Dori';
       dori.lap();
-      if(dori.stroke===5) {
-        $('.winPopUp').show(100).append('<h2>Congrats!<br>' + dori.name + ' wins!</h2>');
+      if(dori.stroke===23) {
+        $('.winPopUp').show(100).append("<h2>Congrats!<br>" + dori.name + " wins!</h2><br><a class='btn reset' href='index.html'><strong>New Game<strong></a>");
         $('.swimOne').stop();
       }
     }
   });
+  //new game button appears upon win screen
   $('.reset').on("click", function(){
     $('.btn').append('');
   });
 });
 
+//fish object
 function Fish() {
   this.stroke = 0;
   this.name = '';
