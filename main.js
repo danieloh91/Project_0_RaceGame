6f1f2ec486cfcbff8b56f4bc961f4a51c90362b4
danieloh91 <daniel.oh.91@gmail.com>
2016-07-11 10:58:16 -0700

    resolve continued fish movemnet post win

--- main.js
+++ main.js
@@ -8,19 +8,34 @@ $(document).on('ready', function(){
         $(".choosePlayers").addClass('hide').slideDown(0);
       });
     }
-    visible = ! visible;
+    visible = !visible;
     //fishes appear after button hides
     $('.fishes').show(500).slideDown(100);
     //player 2 fish starts to move
-    $('.swimTwo').animate({"margin-left": "+=90%"}, 9900);
+    $('.swimTwo').animate({"margin-left": "+=90%"}, 4000);
+    function botWin(){
+        $('.winPopUp').show(100).append("<h2>Congrats!<br>" + dori.name + " wins!</h2><br><a class='btn reset' href='index.html'><strong>New Game<strong></a>");
+        $('.swimOne').stop();
+        $('.swimTwo').stop();
+        $('.swimOne').clearQueue();
+        $('.swimTwo').clearQueue();
+        $("body").off('keypress');
+    }
+    if($('.swimTwo').css("margin-left" === "90%")) {
+      botWin();
+    }
+    // or maybe moving a smaller increments and add a counter and when that counter reaches a certain number, alert dori wins
+    // add an event listener for dori's win...need condition where dori's
+    // jquery.css maybe if the element has a css width of 90 then you can alert that dori won
   });
+
   $(".select2Player").click(function(){
     if (visible) {
       $(".choosePlayers").slideUp('slow',function(){
         $(".choosePlayers").addClass('hide').slideDown(0);
       });
     }
-    visible = ! visible;
+    visible =!visible;
     $('.fishes').show(500).slideDown(100);
   });
 
@@ -33,18 +48,25 @@ $(document).on('ready', function(){
       $('.swimOne').animate({"margin-left": "+=4%"}, "fast" );
       nemo.name = 'Nemo';
       nemo.lap();
-      if(nemo.stroke===23) {
+      if(nemo.stroke===27) {
         $('.winPopUp').show(100).append("<h2>Congrats!<br>" + nemo.name + " wins!</h2><br><a class='btn reset' href='index.html'><strong>New Game<strong></a>");
         $('.swimOne').stop();
         $('.swimTwo').stop();
+        $('.swimOne').clearQueue();
+        $('.swimTwo').clearQueue();
+        $("body").off('keypress');
       }
     } else if (event.keyCode === 47) {
       $('.swimTwo').animate({"margin-left": "+=4%"}, "fast" );
       dori.name = 'Dori';
       dori.lap();
-      if(dori.stroke===23) {
+      if(dori.stroke===27) {
         $('.winPopUp').show(100).append("<h2>Congrats!<br>" + dori.name + " wins!</h2><br><a class='btn reset' href='index.html'><strong>New Game<strong></a>");
         $('.swimOne').stop();
+        $('.swimTwo').stop();
+        $('.swimOne').clearQueue();
+        $('.swimTwo').clearQueue();
+        $("body").off('keypress');
       }
     }
   });
@@ -52,6 +74,8 @@ $(document).on('ready', function(){
   $('.reset').on("click", function(){
     $('.btn').append('');
   });
+  //when someone wins, document.off('keypress') to take off all of the keypress handlers
+  // setTimeout
 });
 
 //fish object
@@ -62,3 +86,8 @@ function Fish() {
     this.stroke++;
   };
 }
+
+//TODO:
+// determine how to show that dori wins during 1 player mode
+// add in a 3-2-1 counter that appears then disappears
+// opens to a game board screen that chooses 1 or 2 player mode with option to choose nemo or dori vs just nemo on 1 player
